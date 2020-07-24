9a4cebf836298771b0f43914ffd0f7ad4dedc024
adriatiq <adrian.anderson@gmail.com>
2020-07-24 16:15:42 +1000

    added gitignore that excludes the wp-config.php file

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'adriatiq_skillz' );
+define( 'DB_NAME', 'skilltrainer' );
 
 /** MySQL database username */
-define( 'DB_USER', 'adriatiq_adriatiq' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'FW^56Ea6d*q.' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
